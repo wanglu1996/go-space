b2c1d7dc6e248bce410f91104a84e9d827d248cc
wanglu1996 <wanglu_233@163.com>
2024-07-10 18:43:02 +0800

    feat: 提交yara格式化工具代码

--- tool/parseYaraRule/main.go
+++ tool/parseYaraRule/main.go
@@ -4,17 +4,10 @@ import (
 	"fmt"
 )
 
-func showHelp() {
-
-}
-
-func showInfo() {
-	fmt.Println("这是一个yara规则格式化工具")
-	fmt.Println("  1.将yara规则中的中文替换")
-	fmt.Println("  2.将字符数超过128字符的进行替换")
-}
-
 func main() {
-	showInfo()
+	app := newApp()
+	if err := app.Execute(); err != nil {
+		fmt.Println(err)
+	}
 
 }
--- tool/parseYaraRule/parse.go
+++ tool/parseYaraRule/parse.go
@@ -1,13 +1,58 @@
 package main
 
-func replaceChinese() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-}
+var ruleNum = 0
 
-func replaceRuleIdentifierLogThan128() {
-
+func replace() string {
+	ruleNum++
+	ret := "rule rule_replace_" + strconv.Itoa(ruleNum) + "{"
+	return ret
 }
 
 func parse(srcFile, dstFile string) {
+	file, err := os.Open(srcFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
+	outFile, err := os.Create(dstFile)
+	if err != nil {
+		fmt.Println("Error creating target file:", err)
+		return
+	}
+	defer outFile.Close()
+	writer := bufio.NewWriter(outFile)
+
+	scanner := bufio.NewScanner(file)
+
+	target := "rule "
+	lineNum := 0
+	for scanner.Scan() {
+		lineNum++
+		line := scanner.Text()
+
+		if strings.Contains(line, target) && strings.Contains(line, "{") {
+			str := replace()
+			line = str
+		}
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			fmt.Println("Error writing to target file:", err)
+			return
+		}
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing writer:", err)
+		return
+	}
 }
